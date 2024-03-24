e9ef5153099aa7c44000b7bf81de042a2fa31543
Lucas Boustead <150277220+lboustead@users.noreply.github.com>
2024-03-23 19:47:55 -0500

    Update settings.py

--- fitness_project/fitness_project/settings.py
+++ fitness_project/fitness_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%of&=88hg*&yomu@tya#4k=ep#7p-w(w2+s(1boohqkhk9=h_(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lucasboustead.pythonanywhere.com']
 
 
 # Application definition
